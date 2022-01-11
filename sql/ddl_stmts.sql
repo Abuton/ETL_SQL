d576ff818a7ae0927346ff81175fb1e00cca54b2
Abuton <alaroabubakarolayemi@yahoo.com>
2022-01-11 22:12:44 +0100

    added aggregation for final table

--- sql/ddl_stmts.sql
+++ sql/ddl_stmts.sql
@@ -45,5 +45,78 @@ INSERT INTO customer_courier_chat_messages (sender_app_type, customer_id, from_i
                                            ('Customer iOS', 17071099, 16293039, 17071099, FALSE, 59528555, 'ARRIVING', 16293039, '2019-08-19 8:01:04'),
                                            ('Customer iOS', 17071099, 17071099, 16293039, FALSE, 59528555, 'PICKING_UP', 16293039, '2019-08-19 8:00:04'),
                                            ('Courier Android', 12874122, 18325287, 12874122, TRUE, 59528038, 'ADDRESS_DELIVERY', 18325287, '2019-08-19 7:59:33');
-                                           
-                                           
+
+
+INSERT INTO orders (order_id, city_code) VALUES (59528555, 'REL'), (59528038, 'BAR'), (59528897, 'BEL');
+INSERT INTO customer_courier_conversations (order_id, city_code, first_courier_messsage, first_customer_messsage, num_messages_courier, num_messages_customer, first_message_by, conversation_started_at, first_responsetime_delay_seconds, last_message_time, last_message_order_stage)
+                                         VALUES
+                                         (59528555, 'REL', 'Hi! I will soon get to your place.' 'Hello, Where are you?', 8, 20, 'Customer', '2019-08-19 7:59:33', '1:00:33'),
+                                         (),
+                                         (),
+                                         ();
+-- test tables
+SELECT COUNT(*) FROM customer_courier_chat_messages;
+
+-- SELECT sender_app_type, customer_id, from_id, order_id, order_stage from customer_courier_chat_messages LIMIT 2;
+
+-- SELECT EXISTS (
+ --  SELECT FROM information_schema.tables 
+ --  WHERE table_name = 'customer_courier_conversations'
+  -- );
+   
+-- SELECT 
+ --  table_name, 
+ --  column_name, 
+ --  data_type 
+-- FROM 
+ --  information_schema.columns
+-- WHERE 
+   -- table_name = 'customer_courier_conversations';
+   
+-- SELECT * FROM customer_courier_conversations;
+
+
+
+WITH orders AS (
+  select distinct order_id as orders_id from customer_courier_chat_messages
+),
+number_of_message_courier as (
+  select order_id, count(sender_app_type) as number_msg_courier from customer_courier_chat_messages where substring(sender_app_type, 1,7) = 'Courier' group by order_id
+),
+number_of_message_customer as (
+  select order_id, count(sender_app_type) as number_msg_customer from customer_courier_chat_messages where substring(sender_app_type, 1,8) = 'Customer' group by order_id
+),
+first_courier_msg as (
+  select order_id, min(message_sent_time) as first_courier_msg from customer_courier_chat_messages where substring(sender_app_type, 1,7) = 'Courier' group by order_id
+),
+first_customer_msg as (
+  select order_id, min(message_sent_time) as first_customer_msg from customer_courier_chat_messages where substring(sender_app_type, 1,8) = 'Customer' group by order_id
+),
+first_msg_by as (
+  select order_id, substring(sender_app_type, 1,8)  as first_msg_by from customer_courier_chat_messages where message_sent_time = (select min(message_sent_time) from customer_courier_chat_messages) group by order_id,sender_app_type
+),
+conversation_started_by as (
+  select order_id, min(message_sent_time) as conversation_started_at from customer_courier_chat_messages group by order_id
+),
+-- fist_response_time
+last_message_time as (
+  select order_id, max(message_sent_time) as last_message_time from customer_courier_chat_messages group by order_id
+),
+last_msg_order_stage as (
+  select order_id, order_stage as last_message_order_stage from customer_courier_chat_messages where message_sent_time = (select max(message_sent_time) from customer_courier_chat_messages) group by order_id, order_stage
+)
+
+INSERT INTO customer_courier_conversations
+ (order_id, first_courier_messsage, first_customer_messsage, num_messages_courier, num_messages_customer, first_message_by, conversation_started_at, last_message_time, last_message_order_stage)
+  
+SELECT o.orders_id, first_courier_msg, first_customer_msg, number_msg_courier, number_msg_customer, first_msg_by, conversation_started_at, last_message_time, last_message_order_stage FROM
+  orders o join number_of_message_courier on o.orders_id = number_of_message_courier.order_id 
+    join number_of_message_customer on number_of_message_customer.order_id = o.orders_id 
+    join first_courier_msg on first_courier_msg.order_id = o.orders_id
+    join first_customer_msg on first_customer_msg.order_id = o.orders_id
+    join first_msg_by on first_msg_by.order_id = o.orders_id
+    join conversation_started_by on conversation_started_by.order_id = o.orders_id
+    join last_message_time on last_message_time.order_id = o.orders_id
+    join last_msg_order_stage on last_msg_order_stage.order_id = o.orders_id;
+    
+select * from customer_courier_conversations;
